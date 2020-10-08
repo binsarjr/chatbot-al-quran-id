9fe9d0e51d37286a6713af03cb4ada10bab6df7b
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2020-10-08 23:31:57 +0700

    Menghapus tag html pada dataset

--- src/resources/data.ts
+++ src/resources/data.ts
@@ -1,11 +1,13 @@
 import Axios from "axios";
+import striptags from "striptags";
 import fs from "fs";
 
 
-;(async () => {
+;(async () => { 
     let resp = await Axios.get('https://al-quran-8d642.firebaseio.com/data.json')
     let data = resp.data.map((data: any) => {
         data.nomor = parseInt(data.nomor)
+        data.keterangan = striptags(data.keterangan)
         return data
     })
     // console.log(data)
