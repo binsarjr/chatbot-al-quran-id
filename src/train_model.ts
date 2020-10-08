a955fb7a51d78ff256f6e0008d0b95e82fcb9ef9
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2020-10-08 22:43:46 +0700

    train data untuk menentukan maksut dari pengguna

--- src/train_model.ts
+++ src/train_model.ts
@@ -21,11 +21,51 @@ data.forEach((data: any) => {
     );
 });
 
+
+
 manager.addDocument('id', `saya ingin baca surat %${namedEntities}%`, 'surat');
+manager.addDocument('id', `saya ingin baca surat %${namedEntities}% ayat %number% sampai %number% `, 'surat');
 manager.addAnswer('id', 'surat', `surat`);
 
 ;(async () => {
     await manager.train();
-    let result = await manager.process('saya ingin baca surat annas dari ayat 1 sampe 5'.toLowerCase())
-    console.log(result)
+
+    let tanyain: any[] = ['surat An-Nas ayat 1 sampai 5','surat annas','surat alfatiha','surat alfatihah ayat 1 sampai 4'];
+    for(let tanya of tanyain) {
+        let result = await manager.process(tanya.toLowerCase())
+    
+        // console.log(result)
+    
+        let targetMsg = ''
+    
+        for(let i in result.entities) {
+            let entities = result.entities
+            if(entities[i].entity == namedEntities) {
+                targetMsg += `surat ${entities[i].option}`
+    
+                if(
+                    typeof entities[parseInt(i)+1] !== 'undefined'
+                    &&
+                    entities[parseInt(i)+1].entity == 'number'
+                ) {
+                    let ayatPrefix = entities[parseInt(i)+1];
+                    targetMsg += ` ayat ke-${ayatPrefix.resolution.value}`
+                }
+    
+                if(
+                    typeof entities[parseInt(i)+2] !== 'undefined'
+                    &&
+                    entities[parseInt(i)+2].entity == 'number'
+                    && 
+                    entities[parseInt(i)+1].resolution.value != entities[parseInt(i)+2].resolution.value
+                ) {
+                    let ayatSuffix = entities[parseInt(i)+2];
+                    targetMsg += ` sampai ayat ke-${ayatSuffix.resolution.value}`
+                }
+    
+            }
+        }
+    
+        console.log(targetMsg)
+    }
 })()
