f823ec3656a83ab865a398a5ff232cfbef089c15
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2020-10-08 21:06:34 +0700

    menambahkan dataset

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import fs from "fs";
 ;(async () => {
 for (let i = 1; i <= 144; i++) {
     let resp = await axios.get(`https://al-quran-8d642.firebaseio.com/surat/${i}.json?print=pretty`)
-    let filename = `dataset/alquran/surat/${i}.json`
+    let filename = `dataset/alquran/surat/${('000' + i.toString()).slice(-3)}.json`
 
     let data: any[] = resp.data.map((data: any) => {
         data.nomor = parseInt(data.nomor)
